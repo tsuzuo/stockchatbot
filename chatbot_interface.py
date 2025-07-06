3ba81eb5bb5000b4d40ad9fad0e3a0ba38ae3774
tsuzuo <72037371+tsuzuo@users.noreply.github.com>
2025-07-06 23:38:18 +0800

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import appdirs as ad
-ad.user_cache_dir = lambda *args: "/tmp"
 import yfinance as yf
 import time
 import random
