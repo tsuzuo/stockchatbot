5804ff6063416f5a9dff12f194220c46965bbb2e
tsuzuo <72037371+tsuzuo@users.noreply.github.com>
2025-07-06 23:35:47 +0800

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import appdirs as ad
-ad.user_cache_dir = lambda *args: "/tmp"
+import plotly.express as px
 import yfinance as yf
 from datetime import datetime
 import time
